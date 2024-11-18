b3e0f0d1903fd068f21cb261ba365f83d8b2874c
Lalith Hakari <lalith.axlrose@gmail.com>
2024-11-18 23:33:16 +0530

    mowed files

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,55 +1,52 @@
-<div>
-    <footer class="relative overflow-hidden py-24 sm:py-32">
-        <div class="relative mx-auto flex w-full max-w-2xl flex-col items-start justify-between gap-x-16 gap-y-12 px-6 lg:max-w-7xl lg:flex-row">
-            <div class="lg:w-1/2">
-                <h2 class="text-2xl font-bold lg:text-[28px]">Subscribe to our newsletter</h2>
-                <div class="relative mt-10">
-                    <div class="relative overflow-hidden rounded-lg border border-gray-100 bg-white p-8 shadow-card">
-                        <span class="absolute inset-y-0 left-0 w-1 bg-green-600"></span>
-                        <div>
-                            <form>
-                                <div class="flex w-full flex-wrap items-stretch gap-4">
-                                    <label class="relative flex min-w-[240px] flex-1 items-center bg-white">
-                                        <span class="sr-only">Email</span>
-                                        <img src="https://picperf.io/https://laravel-news.com/images/icons/newsletter.svg" alt="Newsletter icon" class="pointer-events-none absolute left-3 top-3">
-                                        <input type="text" class="w-full rounded-lg border-gray-100 bg-transparent px-12 py-3 text-gray-600 placeholder-gray-600/50 transition focus:border-gray-100 focus:bg-gray-100/40 focus:outline-none focus:ring-2 focus:ring-green-600/80 focus:ring-offset-2" placeholder="Email">
-                                    </label>
-                                    <button type="submit" class="inline-flex items-center justify-center leading-none bg-green-600 border border-transparent rounded-lg font-bold text-base text-white hover:bg-green-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600/80 focus-visible:ring-offset-2 disabled:bg-green-600/50 disabled:cursor-not-allowed transition ease-in-out duration-300 px-6 py-4">
-                                        Join free
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-10 flex flex-wrap items-center gap-8">
-                    <p class="font-bold">and follow us on</p>
-                    <div class="flex flex-wrap items-center gap-2 sm:flex-nowrap">
-                        <x-common.svg-icon icon="solid/linkedin" title="Dark Mode" class="w-7 h-7" />
-                        <x-common.svg-icon icon="solid/youtube" title="Dark Mode" class="w-7 h-7" />
+<footer class="overflow-hidden py-24 sm:py-32">
+    <div class="mx-auto flex w-full max-w-2xl flex-col items-start justify-between gap-x-16 gap-y-12 px-6 lg:max-w-7xl lg:flex-row">
+        <div class="lg:w-1/2">
+            <h2 class="text-2xl font-bold lg:text-[28px]">Subscribe to our newsletter</h2>
+            <div class="mt-10">
+                <div class="overflow-hidden rounded-lg border border-gray-100 bg-white p-8 shadow-card">
+                    <span class="absolute inset-y-0 left-0 w-1 bg-green-600"></span>
+                    <div>
+                        <form>
+                            <div class="flex w-full flex-wrap items-stretch gap-4">
+                                <label class="flex min-w-[240px] flex-1 items-center bg-white">
+                                    <span class="sr-only">Email</span>
+                                    <input type="text" class="w-full rounded-lg border-gray-100 bg-transparent px-12 py-3 text-gray-600 placeholder-gray-600/50 transition focus:border-gray-100 focus:bg-gray-100/40 focus:outline-none focus:ring-2 focus:ring-green-600/80 focus:ring-offset-2" placeholder="Email">
+                                </label>
+                                <button type="submit" class="inline-flex items-center justify-center leading-none bg-green-600 border border-transparent rounded-lg font-bold text-base text-white hover:bg-green-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600/80 focus-visible:ring-offset-2 disabled:bg-green-600/50 disabled:cursor-not-allowed transition ease-in-out duration-300 px-6 py-4">
+                                    Join free
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-            <div class="grid w-full max-w-sm gap-x-12 gap-y-4 sm:grid-cols-2">
-                <a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600/80 p-1 font-bold hover:text-gray-600" href="/links">
-                    Laravel Links
-                </a>
-                <a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600/80 p-1 font-bold hover:text-gray-600" href="/links">
-                    Laravel Links
-                </a>
-                <a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600/80 p-1 font-bold hover:text-gray-600" href="/links">
-                    Laravel Links
-                </a>
-                <a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600/80 p-1 font-bold hover:text-gray-600" href="/links">
-                    Laravel Links
-                </a>
+            <div class="mt-10 flex flex-wrap items-center gap-8">
+                <p class="font-bold">and follow us on</p>
+                <div class="flex flex-wrap items-center gap-2 sm:flex-nowrap">
+                    <x-svg-icon icon="solid/linkedin" title="Dark Mode" class="w-7 h-7" />
+                    <x-svg-icon icon="solid/youtube" title="Dark Mode" class="w-7 h-7" />
+                </div>
             </div>
         </div>
-        <div class="relative mx-auto mt-20 flex w-full max-w-2xl flex-wrap justify-between gap-x-16 gap-y-12 px-6 lg:max-w-7xl">
-            <p class="text-sm text-gray-600">
-                &copy; {{config('app.name')}}
-                <br>
-            </p>
+        <div class="grid w-full max-w-sm gap-x-12 gap-y-4 sm:grid-cols-2">
+            <a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600/80 p-1 font-bold hover:text-gray-600" href="/links">
+                Laravel Links
+            </a>
+            <a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600/80 p-1 font-bold hover:text-gray-600" href="/links">
+                Laravel Links
+            </a>
+            <a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600/80 p-1 font-bold hover:text-gray-600" href="/links">
+                Laravel Links
+            </a>
+            <a class="inline-flex rounded-sm transition duration-300 leading-none focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600/80 p-1 font-bold hover:text-gray-600" href="/links">
+                Laravel Links
+            </a>
         </div>
-    </footer>
-</div>
+    </div>
+    <div class="relative mx-auto mt-20 flex w-full max-w-2xl flex-wrap justify-between gap-x-16 gap-y-12 px-6 lg:max-w-7xl">
+        <p class="text-sm text-gray-600">
+            &copy; {{config('app.name')}}
+            <br>
+        </p>
+    </div>
+</footer>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,32 +1,32 @@
-<div class="nav container mx-auto">
-    <div class="nav-menu flex justify-between px-8 py-4 bg-slate-50 items-center">
+<header class="nav container mx-auto sticky overflow-hidden top-0">
+    <div class="nav-menu flex justify-between px-8 py-4 bg-slate-100 items-center">
         <div class="menu-item">
             <a href="/">
-                <x-common.svg-icon icon="brand-logo" title="{{config('app.name')}}" class="w-12 h-12" />
+                <x-svg-icon icon="brand-logo" title="{{config('app.name')}}" class="w-12 h-12" />
             </a>
         </div>
         <div class="quick-options-wrapper flex gap-8">
             <div class="menu-item">
-                <x-common.svg-icon icon="outline/search" title="Search" class="w-7 h-7" />
+                <x-svg-icon icon="outline/search" title="Search" class="w-7 h-7" />
             </div>
             <div class="menu-item">
-                <x-common.svg-icon icon="outline/code" title="Code Playground" class="w-7 h-7" />
+                <x-svg-icon icon="outline/code" title="Code Playground" class="w-7 h-7" />
             </div>
             <div class="menu-item">
-                <x-common.svg-icon icon="outline/academic-cap" title="Quiz" class="w-7 h-7" />
+                <x-svg-icon icon="outline/academic-cap" title="Quiz" class="w-7 h-7" />
             </div>
             <div class="menu-item">
-                <x-common.svg-icon icon="outline/cart" title="Your Cart" class="w-7 h-7" />
+                <x-svg-icon icon="outline/cart" title="Your Cart" class="w-7 h-7" />
             </div>
             <div class="menu-item">
-                <x-common.svg-icon icon="outline/moon" title="Dark Mode" class="w-7 h-7" />
+                <x-svg-icon icon="outline/moon" title="Dark Mode" class="w-7 h-7" />
             </div>
             <div class="menu-item">
-                <x-common.svg-icon icon="outline/user" title="Your Profile" class="w-7 h-7" />
+                <x-svg-icon icon="outline/user" title="Your Profile" class="w-7 h-7" />
             </div>
         </div>
     </div>
-    <div class="nav-submenu flex mx-auto px-8 py-6 gap-20">
+    <div class="nav-submenu flex mx-auto px-8 py-6 gap-20 bg-white">
         <div class="submenu-item">
             Bestsellers
         </div>
@@ -40,4 +40,4 @@
             Bestsellers
         </div>
     </div>
-</div>
+</header>
